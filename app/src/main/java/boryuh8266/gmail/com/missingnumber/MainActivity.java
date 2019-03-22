c82c5ccc53fa5db5c2394309164b806bc7581996
Boyu.Chen <boyu.chen@pouchen.com>
2019-03-22 15:05:57 +0800

    init

---
+++ app/src/main/java/boryuh8266/gmail/com/missingnumber/MainActivity.java
@@ -0,0 +1,46 @@
+package boryuh8266.gmail.com.missingnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import boryuh8266.gmail.com.missingnumber.adapter.HomeAdapter;
+import boryuh8266.gmail.com.missingnumber.model.Item;
+
+public class MainActivity extends AppCompatActivity implements HomeAdapter.ItemListener {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Item> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            arrayList.add(new Item("Item 1", R.drawable.battle, "#09A9FF"));
+            arrayList.add(new Item("Item 2", R.drawable.beer, "#3E51B1"));
+            arrayList.add(new Item("Item 3", R.drawable.ferrari, "#673BB7"));
+            arrayList.add(new Item("Item 4", R.drawable.jetpack_joyride, "#4BAA50"));
+            arrayList.add(new Item("Item 5", R.drawable.three_d, "#F94336"));
+            arrayList.add(new Item("Item 6", R.drawable.terraria, "#0A9B88"));
+        }
+        HomeAdapter adapter = new HomeAdapter(this, arrayList, this);
+        recyclerView.setAdapter(adapter);
+
+        GridLayoutManager manager = new GridLayoutManager(this, 5, GridLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+    }
+
+
+    @Override
+    public void onItemClick(Item item) {
+
+    }
+}
---
+++ app/src/main/java/boryuh8266/gmail/com/missingnumber/model/CreateMissingNumber.java
@@ -0,0 +1,38 @@
+package boryuh8266.gmail.com.missingnumber.model;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class CreateMissingNumber {
+
+    private int missingNumber;
+    private int[] qArray;
+
+    CreateMissingNumber(int max) {
+        setNewMissingNumber(max);
+    }
+
+    public void setNewMissingNumber(int max) {
+        missingNumber = (int) (Math.random() * max + 1);
+        LinkedList<Integer> initArray = new LinkedList<Integer>();
+        for (int i = 0; i < max; i++) {
+            if ((i+1) == missingNumber) continue;
+            initArray.add(i + 1);
+        }
+        Collections.shuffle(initArray);
+
+        qArray = new int[max - 1];
+        for (int i = 0; i < qArray.length; i++) {
+            qArray[i] = initArray.get(i).intValue();
+        }
+    }
+
+    public int[] getQArray() {
+        return qArray;
+    }
+
+    public int getMissingNumber(){
+        return missingNumber;
+    }
+
+}
